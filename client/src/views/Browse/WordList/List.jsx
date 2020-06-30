68687fd8893a63740a6a8247a349dca7ec3d00f5
Emil Mordarski <mordarskiemil@gmail.com>
2020-06-30 12:09:00 +0200

    added ability to remove word

--- client/src/views/Browse/WordList/List.jsx
+++ client/src/views/Browse/WordList/List.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import axios from 'axios'
 import AddNew from './AddNew'
 
 export default function List(props) {
@@ -7,12 +8,21 @@ export default function List(props) {
     const switchAddForm = e => isAdding ? setIsAdding(false) : setIsAdding(true)
     const renderAddNew = () => isAdding ? <li><AddNew /></li> : ""
 
+    const removeWord = id => {
+        axios.delete(`/api/words/${id}`)
+            .then(() => props.loadWordsFn())
+            .then(() => console.log(`Word ${id} has been deleted`))
+    }
+
+
     const createWordEntry = word =>
-        <li>
+        <li className="entry">
             <span className="pl">{word.pl}</span>
             <span className="en">{word.en}</span>
             <span className="definition">{word.definition}</span>
             <span className="example">{word.example}</span>
+            <span onClick={() => removeWord(word._id)}>X</span>
+
         </li>
 
     return (
